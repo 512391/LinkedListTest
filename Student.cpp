fc97acd799c68b94b175c7b997ec679ad8a87887
512391 <512391@bsd48.org>
2024-12-04 10:26:29 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 #include"Student.h"
 
+/*
+author: Jay Williamson
+date: 12/3/24
+this just the cpp for the student
+  */
+
 using namespace LinkedList;
 using namespace std;
 
+//constructor
 Student::Student()
 {
   return;  
